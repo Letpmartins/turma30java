2c72f2daa0040fa72901269e2a7ba5a3d5aff722
Letícia P. Martins <54192824+Letpmartins@users.noreply.github.com>
2021-10-07 11:39:31 -0300

    Update UsuarioController.java

--- spring/deploy_blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ spring/deploy_blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -5,12 +5,14 @@ import java.util.Optional;
 
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
 import org.generation.blogPessoal.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,12 +26,21 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired 
+	private UsuarioRepository repository;
 
 	@GetMapping("/all")
 	public ResponseEntity <List<Usuario>> getAll(){
 		
 		return ResponseEntity.ok(usuarioService.listarUsuarios());
 		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
 	}
 	
 	@PostMapping("/logar")
